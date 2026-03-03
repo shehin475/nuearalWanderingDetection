ff952311c67912e022372e28f9f721fb02642b1d
shehin <shehin475@gmail.com>
2026-03-04 00:32:00 +0530

    care

--- ml_api.py
+++ ml_api.py
@@ -238,6 +238,18 @@ def update_fcm_token():
 
     return jsonify({"status": "success"})
 
+@app.route("/firebase-config")
+def firebase_config():
+    return jsonify({
+        "apiKey": os.getenv("FIREBASE_API_KEY"),
+        "authDomain": os.getenv("FIREBASE_AUTH_DOMAIN"),
+        "databaseURL": os.getenv("FIREBASE_DATABASE_URL"),
+        "projectId": os.getenv("FIREBASE_PROJECT_ID"),
+        "storageBucket": os.getenv("FIREBASE_STORAGE_BUCKET"),
+        "messagingSenderId": os.getenv("FIREBASE_MESSAGING_SENDER_ID"),
+        "appId": os.getenv("FIREBASE_APP_ID"),
+    })
+
 @app.route("/predict", methods=["POST"])
 def predict():
     d = request.get_json(silent=True)
